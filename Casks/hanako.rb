360c9e1d420bd2a92a5a62218332ca32e8e98833
Elvis Shi <shonetesmith@gmail.com>
2019-06-14 18:25:12 +0900

    Update hanako.rb

--- Casks/hanako.rb
+++ Casks/hanako.rb
@@ -1,5 +1,5 @@
 cask 'hanako' do
-  version '1.0.2'
+  version '1.0.3'
   sha256 '6a1ff3dc43e8e06458d7563f408a98a59846b9160e0877d9ab87665199a094ab'
 
   url 'https://github.com/el-hoshino/hanako/releases/download/v1.0.3/hanako.zip'
